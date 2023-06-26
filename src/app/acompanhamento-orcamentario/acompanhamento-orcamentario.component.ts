79e4c39898e9ea4aaf79e5c230fd46f29610c528
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-06-26 19:54:21 -0300

    Adicionar tabela no componente

--- src/app/acompanhamento-orcamentario/acompanhamento-orcamentario.component.ts
+++ src/app/acompanhamento-orcamentario/acompanhamento-orcamentario.component.ts
@@ -31,6 +31,33 @@ export interface AcompanhamentoOrcamentario {
   pago: string;
 }
 
+export interface AcompanhamentoOrcamentario {
+  id: number;
+  exercicio: string;
+  tipoCaptacao: string;
+  descricaoEsfera: string;
+  descricaoOrgao: string;
+  descricaoUnidadeOrcamentaria: string;
+  descricaoFuncao: string;
+  descricaosubFuncao: string;
+  tituloPrograma: string;
+  tipoPrograma: string;
+  tituloAcao: string;
+  descricaoProduto: string;
+  descricaoUnidademedida: string;
+  descricaolocalizador: string;
+  qtdMetaLoa: string;
+  projetoLei: string;
+  dotacaoInicial: string;
+  autorizado: string;
+  empenhado: string;
+  liquidado: string;
+  reprogramadoFisico: string;
+  reprogramadoFinanceiro: string;
+  realizadoLoa: string;
+  pago: string;
+}
+
 @Component({
   selector: 'app-acompanhamento-orcamentario',
   templateUrl: './acompanhamento-orcamentario.component.html',
