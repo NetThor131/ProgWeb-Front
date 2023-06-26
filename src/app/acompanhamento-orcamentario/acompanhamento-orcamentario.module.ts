e964422d9416fcbfbf4d8e8c0082daf755f1c46d
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-06-26 19:41:58 -0300

    Adicionar modulo para importar componentes do angular material

--- src/app/acompanhamento-orcamentario/acompanhamento-orcamentario.module.ts
+++ src/app/acompanhamento-orcamentario/acompanhamento-orcamentario.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { AppMaterialModule } from '../shared/app-material';
 import { AcompanhamentoOrcamentarioRoutingModule } from './acompanhamento-orcamentario-routing.module';
 import { AcompanhamentoOrcamentarioComponent } from './acompanhamento-orcamentario.component';
 
 @NgModule({
-  imports: [CommonModule, AcompanhamentoOrcamentarioRoutingModule],
+  imports: [
+    CommonModule,
+    AcompanhamentoOrcamentarioRoutingModule,
+    AppMaterialModule,
+    ReactiveFormsModule
+  ],
   declarations: [AcompanhamentoOrcamentarioComponent],
 })
 export class AcompanhamentoOrcamentarioModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-
+import { AppMaterialModule } from './app-material';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, AppMaterialModule],
 })
-export class SharedModule { }
+export class SharedModule {}
