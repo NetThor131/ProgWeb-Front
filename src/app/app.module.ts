9e4991747bc8ca1331b30d29f0eead69417ffa56
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-06-26 19:16:28 -0300

    Adiciona um cabeçalho na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
