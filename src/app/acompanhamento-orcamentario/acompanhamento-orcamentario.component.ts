6e8e82901ef76b17b2326fdd512539e1837e35d3
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-06-26 19:11:41 -0300

    Criar modulo de acompanhamento orcamentario

---
+++ src/app/acompanhamento-orcamentario/acompanhamento-orcamentario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acompanhamento-orcamentario',
+  templateUrl: './acompanhamento-orcamentario.component.html',
+  styleUrls: ['./acompanhamento-orcamentario.component.css']
+})
+export class AcompanhamentoOrcamentarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/acompanhamento-orcamentario/acompanhamento-orcamentario.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AcompanhamentoOrcamentarioComponent } from './acompanhamento-orcamentario.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AcompanhamentoOrcamentarioComponent]
+})
+export class AcompanhamentoOrcamentarioModule { }
