7edf1166e2f746bd1b04ce244606fb760c0c6bdb
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-06-26 20:34:58 -0300

    Adiciona servico que consome a api

--- src/app/acompanhamento-orcamentario/services/acompanhamento-orcamentario.service.ts
+++ src/app/acompanhamento-orcamentario/services/acompanhamento-orcamentario.service.ts
@@ -1,6 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs';
+
+import { AcompanhamentoOrcamentario } from '../acompanhamento-orcamentario.component';
 
 @Injectable()
 export class AcompanhamentoOrcamentarioService {
-  constructor() {}
+  private readonly API = 'api/AcompanhamentoOrcamentario';
+
+  constructor(private httpClient: HttpClient) {}
+
+  list() {
+    return this.httpClient
+      .get<AcompanhamentoOrcamentario[]>(`${this.API}/ObterTodos`)
+      .pipe(
+        first(),
+        tap((result) => console.log(result))
+      );
+  }
 }
