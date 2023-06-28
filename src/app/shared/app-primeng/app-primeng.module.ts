b9baca5080129f7ebd159c4fed76028908b84baa
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-06-28 00:05:33 -0300

    Adiciona novos modulos primeng

--- src/app/shared/app-primeng/app-primeng.module.ts
+++ src/app/shared/app-primeng/app-primeng.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 
 @NgModule({
-  exports: [CardModule, TableModule],
+  exports: [CardModule, TableModule, ButtonModule, InputTextModule],
 })
 export class AppPrimengModule {}
