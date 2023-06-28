94fa65656f6e9c97cb81ee762cb85a33cb7f3fd1
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-06-28 00:05:14 -0300

    Cria formulario para responder as questoes

--- src/app/acompanhamento-orcamentario/form-acompanhamento-orcamentario/form-acompanhamento-orcamentario.component.ts
+++ src/app/acompanhamento-orcamentario/form-acompanhamento-orcamentario/form-acompanhamento-orcamentario.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { AcompanhamentoOrcamentarioService } from '../services';
+import { Observable, catchError, of } from 'rxjs';
+import { AcompanhamentoOrcamentario } from '../acompanhamento-orcamentario.component';
 
 @Component({
   selector: 'app-form-acompanhamento-orcamentario',
   templateUrl: './form-acompanhamento-orcamentario.component.html',
-  styleUrls: ['./form-acompanhamento-orcamentario.component.css']
+  styleUrls: ['./form-acompanhamento-orcamentario.component.css'],
 })
-export class FormAcompanhamentoOrcamentarioComponent implements OnInit {
+export class FormAcompanhamentoOrcamentarioComponent {
+  descricaoFuncao: string = '';
+  descricaosubFuncao: string = '';
+  tituloAcao: string = '';
+  descricaoOrgao: string = '';
+  tituloPrograma: string = '';
+  tipoPrograma: string = '';
+  descricaolocalizador: string = '';
+  descricaoUnidadeOrcamentaria: string = '';
 
-  constructor() { }
+  data$: Observable<AcompanhamentoOrcamentario[]> = of([]);
 
-  ngOnInit(): void {
+  constructor(private service: AcompanhamentoOrcamentarioService) {}
+
+  onBuscar() {
+    let parameters = {
+      descricaoFuncao: this.descricaoFuncao,
+      descricaosubFuncao: this.descricaosubFuncao,
+      tituloAcao: this.tituloAcao,
+      descricaoOrgao: this.descricaoOrgao,
+      tituloPrograma: this.tituloPrograma,
+      tipoPrograma: this.tipoPrograma,
+      descricaolocalizador: this.descricaolocalizador,
+      descricaoUnidadeOrcamentaria: this.descricaoUnidadeOrcamentaria,
+    };
+
+    this.data$ = this.service.listWithParameters(parameters).pipe(
+      catchError((error) => {
+        console.log('Erro ao carregar cursos.');
+        return of([]);
+      })
+    );
   }
 
+  onCancelar() {
+    this.descricaoFuncao = '';
+    this.descricaosubFuncao = '';
+    this.tituloAcao = '';
+    this.descricaoOrgao = '';
+    this.tituloPrograma = '';
+    this.tipoPrograma = '';
+    this.descricaolocalizador = '';
+    this.descricaoUnidadeOrcamentaria = '';
+  }
 }
