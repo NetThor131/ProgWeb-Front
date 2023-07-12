5214851f2b95ac2f806380f2acdf97cb637f49ad
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-07-12 02:09:22 -0300

    Requisicao para obter respostas

--- src/app/perguntas/perguntas.module.ts
+++ src/app/perguntas/perguntas.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppPrimengModule } from '../shared/app-primeng';
 import { PerguntasRoutingModule } from './perguntas-routing.module';
 import { PerguntasComponent } from './perguntas/perguntas.component';
+import { PerguntasService } from './perguntas.service';
 
 @NgModule({
   declarations: [PerguntasComponent],
   imports: [CommonModule, PerguntasRoutingModule, AppPrimengModule],
+  providers: [PerguntasService],
 })
 export class PerguntasModule {}
--- src/app/perguntas/perguntas/perguntas.component.ts
+++ src/app/perguntas/perguntas/perguntas.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PerguntasService } from '../perguntas.service';
+
 @Component({
   selector: 'app-perguntas',
   templateUrl: './perguntas.component.html',
   styleUrls: ['./perguntas.component.css'],
 })
 export class PerguntasComponent implements OnInit {
-  constructor() {}
+  data: any;
+  constructor(private service: PerguntasService) {}
 
   ngOnInit(): void {}
 
-  solution() {
-    console.log('clicado');
+  solution(nrQuestao: number) {
+    this.data = this.service.consulta(nrQuestao);
   }
 }
