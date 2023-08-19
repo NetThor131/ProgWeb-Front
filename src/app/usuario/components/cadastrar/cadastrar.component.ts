fc86f211ccdc790ce3aa674e7075af414d74386e
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2023-08-19 14:55:15 -0300

    Criar servico para login e cadastrto

--- src/app/usuario/components/cadastrar/cadastrar.component.ts
+++ src/app/usuario/components/cadastrar/cadastrar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
-  styleUrls: ['./cadastrar.component.css'],
 })
 export class CadastrarComponent implements OnInit {
   nome: string | undefined;
--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppPrimengModule } from '../shared/app-primeng';
 import { CadastrarComponent } from './components/cadastrar';
 import { EntrarComponent } from './components/entrar';
+import { UsuarioService } from './services';
 import { UsuarioRoutingModule } from './usuario-routing.module';
 
 @NgModule({
   declarations: [EntrarComponent, CadastrarComponent],
   imports: [CommonModule, UsuarioRoutingModule, AppPrimengModule, FormsModule],
+  providers: [UsuarioService],
 })
 export class UsuarioModule {}
